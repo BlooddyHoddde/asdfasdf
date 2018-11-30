723e0ad83ad33d639e9ad5d69d01bc540037b6fe
BlooddyHoddde <45472590+BlooddyHoddde@users.noreply.github.com>
2018-12-01 01:05:59 +0200

    Update mainbot.js

--- mainbot.js
+++ mainbot.js
@@ -117,33 +117,6 @@ client.on('message', (message) => {
     }
 }); 
 
-    client.on('message', message => {
-var prefix = "!!";
-
-    if (message.author.id === client.user.id) return;
-    if (message.guild) {
-   let embed = new Discord.RichEmbed()
-    let args = message.content.split(' ').slice(1).join(' ');
-if(message.content.split(' ')[0] == prefix + 'bc') {
-    if (!args[1]) {
-message.channel.send("**bc <message>**");
-return;
-}
-        message.guild.members.forEach(m => {
-   if (!message.member.hasPermission('ADMINISTRATOR')) return message.reply(`انت لست ادمن`).catch(console.error);
-            var bc = new Discord.RichEmbed()
-            .addField('» السيرفر :', `${message.guild.name}`)
-            .addField('» المرسل : ', `${message.author.username}#${message.author.discriminator}`)
-            .addField(' » الرسالة : ', args)
-            .setColor('#ff0000')
-            // m.send(`[${m}]`);
-            m.send(`${m}`,{embed: bc});
-        });
-    }
-    } else {
-        return;
-    }
-});
       var prefix = "!!";
   const HeRo = new Discord.Client();
   client.on('message', message => {
