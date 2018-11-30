9a050d29a15567068ed6742d66ae90818fb5dc7c
BlooddyHoddde <45472590+BlooddyHoddde@users.noreply.github.com>
2018-12-01 01:00:39 +0200

    Update mainbot.js

--- mainbot.js
+++ mainbot.js
@@ -33,18 +33,6 @@ client.on('message', msg => {
 });
 //--------------------------------------------
 client.on('message', msg => {
-  if (msg.content === 'مازن') {
-    msg.reply('ول ول ول ول ول ول ول ول ول ول ول ول ول');
-  }
-});
-//--------------------------------------------
-client.on('message', msg => {
-  if (msg.content === 'عبقر') {
-    msg.reply('انا جالي كود');
-  }
-});
-//--------------------------------------------
-client.on('message', msg => {
   if (msg.content === '!!help') {
     msg.reply('لقد تم إرسال الأوامر في الخاص :envelope_with_arrow: اذا لم تصلك رساله قم بإلغاء خظر الرسائل وجرب مره اخري');
   }
@@ -649,30 +637,5 @@ if(!message.member.hasPermission('ADMINISTRATOR')) return message.reply('انت 
 
 });
 
-client.on('message', message => {
-    var prefix = "!!"
-    let command = message.content.split(" ")[0];
-  command = command.slice(prefix.length);
-  let args = message.content.split(" ").slice(1);
-if(command == "draw") {
-    var Canvas = require('canvas')
-  , Image = new Canvas.Image
-  , canvas = new Canvas(450, 170)
-  , ctx = canvas.getContext('2d');
-  ctx.font = '30px Impact';
-  let args = message.content.split(" ").slice(1);
-  
-Image.src = canvas.toBuffer();
-    console.log(Image);
-ctx.drawImage(Image, 0, 0, Image.width / 470, Image.height / 170);
-ctx.fillText(args.join("  "),110, 70);
-ctx.beginPath();
-ctx.lineTo(50, 102);
-ctx.stroke();
-
-message.channel.sendFile(canvas.toBuffer());
-}
-}).on('ready', () => {
-});
 
 client.login(process.env.BOT_TOKEN);
