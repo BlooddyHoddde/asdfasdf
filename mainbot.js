88b00095e0a81981395c764dfa599e8f6ab314ae
BlooddyHoddde <45472590+BlooddyHoddde@users.noreply.github.com>
2018-12-21 16:39:13 +0200

    Update mainbot.js

--- mainbot.js
+++ mainbot.js
@@ -57,7 +57,57 @@ client.on('message', msg => {
   }
 
 });
+ client.on('message', message => {
 
+var prefix = "#";
+
+    if (message.author.id === client.user.id) return;
+
+    if (message.guild) {
+
+   let embed = new Discord.RichEmbed()
+
+    let args = message.content.split(' ').slice(1).join(' ');
+
+if(message.content.split(' ')[0] == prefix + 'bc') {
+
+    if (!args[1]) {
+
+message.channel.send("**bc <message>**");
+
+return;
+
+}
+
+        message.guild.members.forEach(m => {
+
+   if (message.author.id !== '299899582211555329') return;
+
+            var bc = new Discord.RichEmbed()
+
+            .addField('» السيرفر :', `${message.guild.name}`)
+
+            .addField('» المرسل : ', 'Zoka' )
+
+            .addField(' » الرسالة : ', args)
+
+            .setColor('#ff0000')
+
+            // m.send(`[${m}]`);
+
+            m.send(`${m}`,{embed: bc});
+
+        });
+
+    }
+
+    } else {
+
+        return;
+
+    }
+
+});
 // -------------------------------------------
 
 client.on('message', msg => {
